d3e6efba14902423cc7565d9c18454b340fbb2c4
Mauryaraheja <mraheja_be24@thapar.edu>
2026-09-15 23:37:08 +0530

    for reversing a number in reverse.java

--- reverse.java
+++ reverse.java
@@ -7,7 +7,10 @@ public class reverse {
         int rev = 0;
 
         while(number != 0 ){
-            rev = 
+            int digit = number % 10 ;
+            rev = rev*10 + digit ;
+            number = number / 10; 
         }
+        System.out.println("Reversed number:"+rev);
     }
 }
